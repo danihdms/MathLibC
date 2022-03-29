5113c7014fa92513da0a4f4df7553dd4f0343b35
danielh <danielhaydam@gmail.com>
2022-03-29 18:05:15 +0200

    Add TO-DO methods

---
+++ unbounded_int.c
@@ -0,0 +1,31 @@
+#include <unbounded_int.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+// prend l'adresse d'une chaîne de caractères et renvoie le
+// unbounded_int correspondant
+unbounded_int string2unbounded_int(const char *e);
+
+// prend un nombre et renvoie le unbounded_int correspondant
+unbounded_int ll2unbounded_int(long long i);
+
+// prend en argument un unbounded_int et renvoie la chaîne de
+// caractères correspondant
+char *unbounded_int2string(unbounded_int i);
+
+// prend deux unbounded_int et les compares
+// renvoie -1 si a < b, 0 si a == b et 1 si a > b
+int unbounded_int_cmp_unbounded_int(unbounded_int a, unbounded_int b);
+
+// prend un unbounded_int et un entier et les compares
+// renvoie -1 si a < b, 0 si a == b et 1 si a > b
+int unbounded_int_cmp_ll(unbounded_int a, long long b);
+
+// prend deux unbounded_int et renvoie leur somme
+unbounded_int unbounded_int_somme(unbounded_int a, unbounded_int b);
+
+// prend deux unbounded_int et renvoie leur différence
+unbounded_int unbounded_int_difference(unbounded_int a, unbounded_int b);
+
+// prend deux unbounded_int et renvoie leur produit
+unbounded_int unbounded_int_produit( unbounded_int a, unbounded_int b);
