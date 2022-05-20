942db357f62cc850953d6e424a4ac318b7e102f5
Celine Ye <celine.ye20@gmail.com>
2022-05-20 22:51:31 +0200

    Solved some issues in calc and added main and gestion of issues. Modified Makefile in consequence, launch.json to dubugg calc and .gitignore to ignore the new executable

--- calc_unbounded_int.c
+++ calc_unbounded_int.c
@@ -1,11 +1,12 @@
 #include "unbounded_int.h"
+#include <string.h>
 #define MAX 5   // le nombre maximum de termes attendu par ligne
 
-typedef struct {
-    paire *suivant;
+typedef struct paire {
+    struct paire *suivant;
     char *nom;
     unbounded_int valeur;
-    paire *precedent;
+    struct paire *precedent;
 } paire;
 
 typedef struct {
@@ -14,6 +15,19 @@ typedef struct {
     size_t len;
 } hashmap;
 
+// Prend une hashmap et un nom et vérifie si la hashmap contient un élément de ce nom
+static int contient_variable(hashmap variables, char *nom) {
+    paire *tmp = variables.premier;
+    while (tmp != NULL) {
+        if (strcmp(tmp->nom, nom) == 0) return 1;
+        tmp = tmp->suivant;
+    }
+    return 0;
+}
+
+// Prend une hashmap, un nom et une valeur :
+//   - si un élément de ce nom existe déjà, change sa valeur
+//   - sinon, l'ajoute dans la hashmap
 static hashmap ajoute_variable(hashmap variables, char *nom, unbounded_int valeur) {
     if (valeur.signe == '*') {
         return variables;
@@ -50,15 +64,7 @@ static hashmap ajoute_variable(hashmap variables, char *nom, unbounded_int valeu
     return variables;
 }
 
-static int contient_variable(hashmap variables, char *nom) {
-    paire *tmp = variables.premier;
-    while (tmp != NULL) {
-        if (strcmp(tmp->nom, nom) == 0) return 1;
-        tmp = tmp->suivant;
-    }
-    return 0;
-}
-
+// Prend une hashmap et un nom et renvoie la paire de ce nom
 static paire *paire_de_nom(hashmap variables, char *nom) {
     paire *tmp = variables.premier;
     while (tmp != NULL) {
@@ -68,6 +74,7 @@ static paire *paire_de_nom(hashmap variables, char *nom) {
     return NULL;
 }
 
+// Prend une hashmap et un nom et renvoie la valeur de l'élément de ce nom
 static unbounded_int *valeur_de(hashmap variables, char *nom) {
     paire *tmp = variables.premier;
     while (tmp != NULL) {
@@ -77,29 +84,33 @@ static unbounded_int *valeur_de(hashmap variables, char *nom) {
     return NULL;
 }
 
-void interpret(FILE *fp) {
+void interpreteur(FILE *fp) {
     hashmap variables = {.premier = NULL, .dernier = NULL, .len = 0};
     char *scan = malloc(sizeof(char));
     while(fscanf(fp, "%s", scan) != -1) {
-        char *string[MAX] = calloc(MAX, sizeof(char));
+        char **string = calloc(MAX, sizeof(char));
         int i = 0;
+        // TODO : chercher un moyen d'avoir la taille exacte du string à créer
         char *value = malloc(sizeof(char));
         while(sscanf(scan, "%s", value) != EOF) {
             string[i] = value;
             i++;
         }
         
-        if (string[0] == "print") {
+        if (strcmp(string[0], "print") == 0) {
             paire *p = paire_de_nom(variables, string[1]);
             if (p != NULL) printf("%s = %s", p->nom, unbounded_int2string(p->valeur));
-        } else if (string[1] == '=') {
-            if (string[3] == '*' || string[3] == '+' || string[3] == '-') {
+        } else if (strcmp(string[0], "=") == 0) {
+            if (strcmp(string[0], "*") == 0 || strcmp(string[0], "+") == 0 || strcmp(string[0], "-") == 0) {
                 unbounded_int a = string2unbounded_int(string[2]);
                 unbounded_int b = string2unbounded_int(string[4]);
+                if (a.signe == '*') a = *valeur_de(variables, string[2]);
+                if (b.signe == '*') b = *valeur_de(variables, string[4]);
+
                 if (a.signe != '*' && b.signe != '*') {
                     unbounded_int res;
-                    if (string[3] == '+') res = unbounded_int_somme(a, b);
-                    else if (string[3] == '-') res = unbounded_int_difference(a, b);
+                    if (strcmp(string[0], "+") == 0) res = unbounded_int_somme(a, b);
+                    else if (strcmp(string[0], "-") == 0) res = unbounded_int_difference(a, b);
                     else res = unbounded_int_produit(a, b);
                     variables = ajoute_variable(variables, string[0], res);
                 }
@@ -107,13 +118,36 @@ void interpret(FILE *fp) {
                 variables = ajoute_variable(variables, string[0], string2unbounded_int(string[2]));
             }
         }
-        // Le tableua value doit être utilisé pour effectuierles calculs et les stocker dans variables
+        // Le tableau value doit être utilisé pour effectuer les calculs et les stocker dans variables
 
         for(int i = 0; i < 5; i++) {
             free(string[i]);
         }
         free(string);
     }
+}
+
+int main(int argc, char *argv[]) {
+    FILE *lecture;
+    FILE *ecriture;
+
+    if (strcmp(argv[1], "-i") == 0) lecture = fopen(argv[1], "r");
+    else if (strcmp(argv[3], "-i") == 0) lecture = fopen(argv[3], "r");
+    // else lecture = stdin;
+
+    // ligne temporaire pour les tests
+    else lecture = fopen("./texte.txt", "r");
+
+    if (strcmp(argv[1], "-o") == 0) ecriture = fopen(argv[1], "w");
+    else if (strcmp(argv[3], "-o") == 0) ecriture = fopen(argv[3], "w");
+    else ecriture = stdout;
+
+    if (lecture == NULL) {
+        perror("Failed to open file.");
+        exit(1);
+    }
+    interpreteur(lecture);
+    fclose(lecture);
 }
 
 //
